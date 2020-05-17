8c4591fff02dbe1c1f145d30d063f9f3da0f351c
Soumya Mondal <s.mondal8@gmail.com>
2020-05-17 15:51:49 +0530

    First commit - has errors , need to fix them.

---
+++ src/main/java/SimpleObservable.java
@@ -0,0 +1,28 @@
+import io.reactivex.Emitter;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class SimpleObservable {
+    public static void main(String[] args) {
+        Observable<String> observable = Observable.just("First","Second","Third");
+        Observer<String> observer = new Observer<String>() {
+            public void onSubscribe(Disposable disposable) {
+
+            }
+
+            public void onNext(String s) {
+                System.out::println;
+            }
+
+            public void onError(Throwable throwable) {
+
+            }
+
+            public void onComplete() {
+
+            }
+        }
+        //Observable<String> observable = Observable.create()
+    }
+}
