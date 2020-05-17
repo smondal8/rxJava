569a723582b1c7d501112b75b87f70b4a34e46aa
Soumya Mondal <s.mondal8@gmail.com>
2020-05-17 21:05:47 +0530

    Rectification made in this commit

--- src/main/java/SimpleObservable.java
+++ src/main/java/SimpleObservable.java
@@ -3,26 +3,36 @@ import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SimpleObservable {
     public static void main(String[] args) {
         Observable<String> observable = Observable.just("First","Second","Third");
+        List<String> lst = Arrays.asList("First","Second","Last");
+        Observable<String> observableList = Observable.fromIterable(lst);
         Observer<String> observer = new Observer<String>() {
             public void onSubscribe(Disposable disposable) {
-
+                System.out.println("Subscription is done");
             }
 
             public void onNext(String s) {
-                System.out::println;
+                System.out.println(s);
             }
 
-            public void onError(Throwable throwable) {:wq
+            public void onError(Throwable throwable) {
+
 
             }
 
             public void onComplete() {
-
+                System.out.println("Completed");
             }
-        }
-        //Observable<String> observable = Observable.create()
+        };
+        observable.subscribe(observer);
+        System.out.println("----------------From list-----------");
+        observableList.subscribe(System.out::println,throwable -> throwable.printStackTrace(),()-> System.out.println("completed"));
+        //emitter pending
     }
 }
